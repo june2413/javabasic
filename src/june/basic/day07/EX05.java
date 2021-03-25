fa3a7adcd8ccba1e2cd87d95e4c759a5daf5e444
june2413 <june2413@naver.com>
2021-03-25 18:03:17 +0900

    클래스 매서드

---
+++ src/june/basic/day07/EX05.java
@@ -0,0 +1,39 @@
+package june.basic.day07;
+
+public class EX05 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Fan {
+        private int SLOW;
+        private int MEDIUM;
+        private int FAST;
+        private String speed; //SLOW
+        private String on;    // false
+        private double radius; // 5.0
+        private String color;  // blue
+
+    public Fan(){ }
+
+        public Fan(int SLOW, int MEDIUM, int FAST, String speed, String on,
+                   double radius, String color  )
+        {
+            this.SLOW = SLOW;
+            this.MEDIUM = MEDIUM;
+            this.FAST = FAST;
+            this.speed = speed;
+            this.on = on;
+            this.radius = radius;
+            this.color = color;
+        }
+
+        }
+
+
+
+
+
+
+    }
+
