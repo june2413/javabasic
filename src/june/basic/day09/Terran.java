eececff428986df51ab1ef5f4f95e18fefa8a54e
june2413 <june2413@naver.com>
2021-03-26 18:21:04 +0900

    추상클래스 메서드 재정의 인터페이스

---
+++ src/june/basic/day09/Terran.java
@@ -0,0 +1,73 @@
+package june.basic.day09;
+
+public class Terran {
+}
+
+//class unit {
+//    protected String name;
+//    protected int hp;
+//    protected int atk;
+//    protected int spd;
+//    protected int mnr;
+//    protected int gas;
+//
+//    public unit() {
+//    }
+//
+//    public unit(String name, int hp, int atk, int spd, int mnr, int gas) {
+//        this.name = name;
+//        this.hp = hp;
+//        this.atk = atk;
+//        this.spd = spd;
+//        this.mnr = mnr;
+//        this.gas = gas;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getHp() {
+//        return hp;
+//    }
+//
+//    public void setHp(int hp) {
+//        this.hp = hp;
+//    }
+//
+//    public int getAtk() {
+//        return atk;
+//    }
+//
+//    public void setAtk(int atk) {
+//        this.atk = atk;
+//    }
+//
+//    public int getSpd() {
+//        return spd;
+//    }
+//
+//    public void setSpd(int spd) {
+//        this.spd = spd;
+//    }
+//
+//    public int getMnr() {
+//        return mnr;
+//    }
+//
+//    public void setMnr(int mnr) {
+//        this.mnr = mnr;
+//    }
+//
+//    public int getGas() {
+//        return gas;
+//    }
+//
+//    public void setGas(int gas) {
+//        this.gas = gas;
+//    }
+//}
