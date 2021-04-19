1e04109a8c5c40a9d3af80413e5abe857258439f
june2413 <june2413@naver.com>
2021-04-19 15:56:35 +0900

    문자단위 스트림 바이트단위 스트림 직렬화/역직렬화

---
+++ src/june/basic/day12/QXX.java
@@ -0,0 +1,18 @@
+package june.basic.day12;
+
+import java.util.Scanner;
+
+public class QXX {
+    public static void main(String[] args) {
+
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("사이의 모든 값의 총합을 구할 두수를 입력해주세요");
+        int num1  = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if ( num1 >= num2){ for(int i =num2; i<=num2;++num2);}
+
+        System.out.println(num2);
+    }
+}
